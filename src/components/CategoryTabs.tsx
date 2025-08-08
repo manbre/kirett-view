06a005979e90d2d46808c28a686af0b8635116ec
manbre~ <mbreitzke@aol.com>
2025-08-08 03:23:58 +0200

    tab terms mark

--- src/components/CategoryTabs.tsx
+++ src/components/CategoryTabs.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Category, CategoryLabels } from "@/constants/category";
+import { useStore } from "@/store/useStore";
 
 type Props = {
   selectedCategory: Category | null;
@@ -8,24 +9,41 @@ type Props = {
 };
 
 export const CategoryTabs = ({ selectedCategory, onChangeCategory }: Props) => {
+  const selectedTerms = useStore((state) => state.selectedTerms);
+
   return (
     <div className="flex h-fit w-screen flex-row pt-1">
       {Object.entries(CategoryLabels)
         // in alphabetical order
         .sort((a, b) => a[1].localeCompare(b[1]))
-        .map(([category, label]) => (
-          <button
-            key={category}
-            onClick={() => onChangeCategory(category as Category)}
-            className={`mx-3 border-b-2 hover:cursor-pointer hover:border-[var(--mark)] ${
-              selectedCategory === category
-                ? "border-[var(--mark)]"
-                : "border-transparent"
-            }`}
-          >
-            {label}
-          </button>
-        ))}
+        .map(([catKey, label]) => {
+          const category = catKey as Category;
+          // check if at least one term is out of the category
+          const hasAny =
+            Array.isArray(selectedTerms[category]) &&
+            selectedTerms[category].length > 0;
+          const isActive = selectedCategory === category;
+
+          return (
+            <button
+              key={category}
+              onClick={() => onChangeCategory(category)}
+              className={`relative mx-3 my-2 border-b-2 hover:cursor-pointer hover:border-[var(--mark)] ${
+                isActive ? "border-[var(--mark)]" : "border-transparent"
+              } `}
+            >
+              {label}
+              {hasAny && (
+                <span
+                  className="absolute top-4 text-xs text-blue-400"
+                  // title={`${selectedTerms[category].length} ausgewählt`}
+                >
+                  {selectedTerms[category].length}
+                </span>
+              )}
+            </button>
+          );
+        })}
     </div>
   );
 };
